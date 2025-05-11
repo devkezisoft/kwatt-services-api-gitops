252842c8cdec5a994506605f7e7f0e691412436f
gtemate <dev.sekhmet@gmail.com>
2025-05-11 19:42:00 +0200

    add terraform

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -1,4 +1,4 @@
-provider "scaleway/scaleway" {
+provider "scaleway" {
   access_key = var.scaleway_access_key
   secret_key = var.scaleway_secret_key
   project_id = var.scaleway_project_id
---
+++ infrastructure/dev/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    scaleway = {
+      source  = "scaleway/scaleway"
+      version = "~> 2.26.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2"
+    }
+  }

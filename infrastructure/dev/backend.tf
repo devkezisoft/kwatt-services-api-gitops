f0b87b3b6d87997a2ba521d357acf43038dac167
gtemate <dev.sekhmet@gmail.com>
2025-05-12 09:21:59 +0200

    add github actions

--- infrastructure/dev/backend.tf
+++ infrastructure/dev/backend.tf
@@ -3,9 +3,10 @@ terraform {
     bucket  = "kwatt-terraform-state"
     key     = "dev/terraform.tfstate"
     region  = "eu-west-3"
-    endpoint = "https://kwatt-terraform-state.s3.fr-par.scw.cloud"
 
+    endpoint                    = "https://s3.fr-par.scw.cloud"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    force_path_style            = true
   }
 }
